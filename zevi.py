3961edb77becf3b8925e1173843b5af31e51e48b
Jhordan Silveira de Borba <39818915+SapoGitHub@users.noreply.github.com>
2018-07-18 18:18:33 -0300

    correção

--- zevi.py
+++ zevi.py
@@ -17,17 +17,26 @@ from oauth2client.service_account import ServiceAccountCredentials  #Biblioteca 
 import datetime                                                     #Biblioteca com funções relacionadas ao tempo
 
 #DADOS SENSÍVEIS----------------------------------------------------------------------------------------------------------------
-token =                 'NDY4ODI1OTAwNzQ2NTM5MDA4.Di_TaQ.kP42Lt9eXkyMfMB_1w2p2YKq9wM'       #Token
-consumer_key =          'BOAs4wfRHO5ShFjojLPzLdK3B'                                         #Consumer Key (API Key)
-consumer_secret =       'BUcAlFriglp6RkLzVvkZ8b3zYUNLtqvAwGGWs7YudLLQWnO1Gi'                #Consumer Secret (API Secret)
-access_token =          '1019318137605513218-UzaJyIKGjSL8EQ6fta4L5BlhnFRytJ'                #Access Token
-access_token_secret=    'M5fauDKnuJt3yC3RdxMM7Wr1NFBzhruuc51DGo21nbSk0'                     #Access Token Secret
+token = os.environ['token']                                 #Token
+consumer_key = os.environ['consumer_key']                   #Consumer Key (API Key)
+consumer_secret = os.environ['consumer_secret']             #Consumer Secret (API Secret)
+access_token = os.environ['access_token']                   #Access Token
+access_token_secret = os.environ['access_token_secret']     #Access Token Secret
+
+#Vamos montar nossa private_key
+var_amb=os.environ["private_key"]   #Recebemos a variável
+dividido=var_amb.split("\\n")       #Dividimos onde tem \n
+chave=""                            #Onde vamos remontar
+for linha in dividido:
+    if (len(linha)>0):              #Nossa última linha é apenas '' e queremos deixar assim
+        chave=chave+linha+"\n"
+
 login = {                                                                                   #Dados do API do Google
-    "type": "service_account",
-    "private_key_id": "3a2c4557871aa329ab0ec6813e94b291a427a9f7",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCzdbgsoRrgfBY6\nYxpP4fpwpqNGIOLUycXcGhcPudt1kb1gAQoxt4rWZz6Uz8SegzNkntWdSiRYG1nI\nAYiMOfIRWiZRAoenfFNKa62QhpMXOjvR9YvtUmK6TrRYJM3vjHv16tSvsi5xdLuD\nGB42mGBMlZejNrdms0SnGibw2oo8rK9s3ARYPrarVj17nKyDIHiNKCc20WWGMVnz\nKTml2JTdNrOmuDEAcAB+5+FI3gZyh9+25dcMSD2fYCG+D51IUiH1M4HCMubxyrht\nMMLYuADsoQKDseFXqv7iLktT8bXsfoCaL71Nqs/1RERp8/1QVRLBP7o/cl69Eb0p\nXSY35F9LAgMBAAECggEACGzwsDKUcxpnL1VMhbzk2iL8J02GGxpy6b/C+iJAhGmm\nMtqggEHGJE5lnW0hykxEx6bRIj1w9mNxxnHu16BJsYsCWIaePSFQPTUG2+AEd8Kj\nPH+sSaU27P2i1dMoiXa7RAoehcha5W3CiWx5hzzO7KK0fssj7aIYUmqxROsu6VJq\nJ9yTH9VwHkS9wPvXVsIj5zaBM6P38z7CZXt66OHfDeslae/ekyUVQ1q+na9N3pVl\nZq0CX/iUk7STifgZBpBz3y9n/XuYL24yCH6oJ7n0hZ4w40Jp2B/4xdJ9+Ep385IE\nIEY/iH9lYMHp19QtcUWyTllM/trL3BMtp1sKj8aWaQKBgQD5NdXCGVde3jxPMo5X\nYRW8CY22iDc8Zaxh4BBpCsRcASR3tvFWG9iLngkj2AvNBirU6CKa4lUKbdOOw6Wt\nHrLXDVOjWrzG5aFI8/aaaHpt2tXgzMRaL7ZUL9jo3crrZ5MdCTMz/eeqanz88R1b\nT6BuqEenOfpgGCFWQTJ2oNzy4wKBgQC4WWWDCtC9iZQXEHXWBmjQ54EW0e+ikC4R\nIJp5FcT6wa4hAjSsA7scAiGtPxMGyyvdzvLJE4sSRSpjzleZVH7ozr+vgCQWiXDo\nOm5fCH+7vgFiKzpiVL0OYx38ID2stm0UxpPgBWGus73z0vpQm5aij31/iuoyySC6\nXxEe5x7GeQKBgQDQSW2CT9nVYZs4ekG7yFn5EmcOM2UvJq9gEnzEvooFd7L6F743\nwQhJEOoEulKKpmfwEKCzoQ2ArPBP9zemrOy+jtXjxzfWaSBXMAhhX4dL/8YYoCxQ\nUGZskJ0TbCRogeXUM9dG0iTpKx2R7xghNDkbo6xXmT6pSCG8zLsXqTViDwKBgFkh\n6CH27bxms7J/I+pKiQ1Qkaq98JZyDcP8NpQLAYWYdZ/CGEN/Pk2pfizszT9ApsIF\nGIA+McSwqnE9SD97iiz4IdgyJcC7zVqLPeg3DMNyd+rGKeF3RT2akNnhoNBMF+uu\no7rx/yvf/hzQynSE9c09gZlUSi4p7ugNpRKC/u9JAoGBAKZmfIGrrdCjYScJqmnf\n0yW4audV9gI2RW6s5sP2hrMgGdr+oP+5NrFBK3V0h+QLRNWkBJU3e+NTU2czyhsL\nyDqRC1a5kLE1CPk5IDoJLPjBqZzv3Kzz/S6/Zu2GLZMiWeAUKYlZRYqDIOKr55Us\n8sJUb57LamBUu4V2Iz0sv2kJ\n-----END PRIVATE KEY-----\n",
-    "client_email": "sapo-876@ze-vi-1531877436506.iam.gserviceaccount.com",
-    "client_id": "112542471157463643966",
+    "type": os.environ['type'],
+    "private_key_id": os.environ['private_key_id'],
+    "private_key": chave,
+    "client_email": os.environ['client_email'],
+    "client_id": os.environ['client_id'],
     }
 
 #CONFIGURAÇÃO DISCORD---------------------------------------------------------------------------------------------------------
@@ -121,7 +130,7 @@ async def opina(*assunto):
     
 
 #Descrição do comando da aposta:
-descri="Faça apostas nos jogos do dia da liga Overwatch usando uma palavra para cada time e com espaço entre os elementos.\nEx.: shangai 3 x 0 gladiators.\n\nSinônimos:\n"
+descri="Faça apostas nos jogos do dia da liga Overwatch usando uma palavra para cada time e com espaço entre os elementos, com a data opcional se não for no dia.\nEx.: shanghai 3 x 0 gladiators 18/07.\n\nSinônimos:\n"
 descri=descri+"Shanghai Dragons: shanghai,dragons,xangai dragons\n"
 descri=descri+"Los Angeles Gladiators: gladiators\n"
 descri=descri+"San Francisco Shock: schock,sf\n"
@@ -139,11 +148,12 @@ descri=descri+"Philadelphia Fusion: philadelphia,fusion"
                 brief="Apostas do dia na Overwatch League", #Descrição que aparece no !help
                 aliases=[ 'liga','ow','overwatch'], #Outras formas de chamar a mesma função
                 pass_context=True)  #Se vai passar o contexto
-async def aposta(context,time1,placar1,x,placar2,time2):
+async def aposta(context,time1,placar1,x,placar2,time2,*data):
     #context    - Informações sobre a mensagem que foi enviada.
 
     apostador=str(context.message.author) #Para sabermos quem esta apostando
-    
+
+
     #Dicionários que vamos utilizar
     dsem = {"1":"SEG","2":"TER","3":"QUA","4":"QUI","5":"SEX","6":"SAB","7":"DOM"} #Dias da semana
     times = {   #Sinônimos para os nomes dos times
@@ -159,8 +169,12 @@ async def aposta(context,time1,placar1,x,placar2,time2):
         "Philadelphia Fusion":("philadelphia","fusion")}
     nos={"ZéRomildo#1325":5,"Fitz#0746":6,"Sapo#0431":7}    #Nossa localização na tabela
 
-    #Vamos pegar o dia atual
-    dia = dsem[datetime.date.today().strftime("%w")]+ " - " + datetime.date.today().strftime("%d") +"/"+datetime.date.today().strftime("%m")
+    if (len(data)==0):  #Se não passamos nenhuma data, é no dia de hoje a aposta
+        dia = dsem[datetime.date.today().strftime("%w")]+ " - " + datetime.date.today().strftime("%d") +"/"+datetime.date.today().strftime("%m")
+    else:               #Se não, usamos a data informada
+        dados=data[0].split("/")       #Vamos dividir entre dia e mês
+        data=datetime.date(2018, int(dados[1]), int(dados[0]))  #Construir uma data a partir disto
+        dia = dsem[data.strftime("%w")]+ " - " + data.strftime("%d") +"/"+data.strftime("%m") #E salvar
 
     timeshj=["",""]     #Onde vamos guardar os dois times que foram passados
     #Precisamos pegar o nome correto do primeiro time
