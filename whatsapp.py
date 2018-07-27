dd3484512b8b1c41c5e45cd50fd51f7ccf6ad55e
Jhordan Silveira de Borba <39818915+SapoGitHub@users.noreply.github.com>
2018-07-27 16:50:12 -0300

    ok

--- whatsapp.py
+++ whatsapp.py
@@ -36,36 +36,40 @@ def whatsapp_mensagens(driver,tamanho_max):
     #driver         - Conexão com o Chrome
     #tamanho_max    - Quantidade de contatos e grupos
 
-    #try:
-    contatos=novas_msgs(driver,tamanho_max)
+    #try:           #Tentamos checar
+    contatos=novas_msgs(driver,tamanho_max) #Pegamos quem nos enviou novas mensagens
     print(contatos)
-    text='Novas mensagens de: '
+    text='Novas mensagens de: '             #Montamos nosso texto
     for contato in contatos:
         texto=texto+contato+', '
 
-    if (len(contatos)>0):
-        return texto
+    if (len(contatos)>0):                   #Se alguém nos enviou
+        return texto                        #Retornamos a mensagem
     else:
-        return ('Sem novas mensagens.')
+        return ('Sem novas mensagens.')     #Ou avisamos que não há novas
     #except:
-    #   return ('Tente de novo.')
+    #   return ('Tente de novo.')           #Ou ainda avisamos que não conseguimso checar
 
 
+#Função para checar as últimas mensagens sem respostas de algum contato
 def whatsapp_contato(driver,contato):
-    try:
-        mensagens=ult_msgs(driver,contato)
+    #driver         - Conexão com o Chrome
+    #contato        - De quem queremos ver que mensagens recebemos
+
+    try:                                        #Vamos tentar
+        mensagens=ult_msgs(driver,contato)      #Recebemos as últimas mensagens do contato
         driver.get("https://web.whatsapp.com")  #Reabrimos a pagina para não ficar em nenhuma conversa aberta
 
-        texto=contato+':\n'
+        texto=contato+':\n'                     #Vamos montar as mensagens
         tam=len(mensagens)
         for n in range(tam-1,-1,-1):
             texto=texto+mensagens[n]+'\n'
         
-        if (len(mensagens)>0):
-            return texto
-        else:
+        if (len(mensagens)>0):                  #Se temos mensagens não respondidas
+            return texto                        #Retornamos as mensagens
+        else:                                   #Ou então retornamos que não há mensagens
             return ('Sem mensagens não respondidas.')
     except:
-        return ('Tente de novo.')
+        return ('Tente de novo.')               #Ou ainda avisamos que não conseguimos
 
 print ('Módulo do WhatsApp importado.')
